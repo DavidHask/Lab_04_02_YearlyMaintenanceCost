b5ca99bb292cf7b9ebc8165faeb86b4f4eb70c5f
DavidHask <haskeldd@mail.uc.edu>
2024-09-20 11:37:56 -0400

    Lab 4, second task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        int summerCost = 500;
+        int fallCost = 350;
+        int winterCost = 550;
+        int springCost = 250;
+        int totalMaintenance = summerCost + fallCost + winterCost + springCost;
 
+        System.out.println("The maintenance cost for summer was " + summerCost +
+                ",\nthe maintenance cost for fall was " + fallCost +
+                ",\nthe maintenance cost for winter was " + winterCost +
+                ",\nthe maintenance cost for spring was " + springCost +
+                ",\nmaking the total maintenance for the year " + totalMaintenance);
     }
 }
